46fae600e587d07aa0220d947291a46f3ce778de
usamaeltmsah <usamaeltmsah@gmail.com>
2020-11-01 20:48:54 +0200

    Increase the value when press the increase button

--- Bullseys/ViewController.swift
+++ Bullseys/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var counter = 0
+    @IBOutlet weak var numLabel: UILabel!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func increase(_ sender: Any) {
+        counter += 1
+        numLabel.text = "\(counter)"
+    }
 }
 
